a4e7ffa74ab45d961b9014498b9ca48f5b6799be
Dexin.Zhang <dexin.zhang@noaa.gov>
2019-06-18 15:22:26 +0000

    Unified build (20190618) script/makefile sp

--- build_sp.sh
+++ build_sp.sh
@@ -1,37 +1,6 @@
 #!/bin/sh
 
- (( $# == 0 )) && {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
-
- sys=${1,,}
- [[ $sys == wcoss || $sys == dell || $sys == cray ||\
-    $sys == theia || $sys == intel_general || $sys == gnu_general ]] || {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
- debg=false
- inst=false
- skip=false
- local=false
- (( $# > 1 )) && {
-   [[ ${2,,} == build ]] && debg=false
-   [[ ${2,,} == debug ]] && debg=true
-   [[ ${2,,} == install ]] && inst=true
-   [[ ${2,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${2,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${2,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
- (( $# > 2 )) && {
-   [[ ${3,,} == build ]] && debg=false
-   [[ ${3,,} == debug ]] && debg=true
-   [[ ${3,,} == install ]] && inst=true
-   [[ ${3,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${3,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${3,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
-
+ source ./Conf/Analyse_args.sh
  source ./Conf/Collect_info.sh
  source ./Conf/Gen_cfunction.sh
  source ./Conf/Reset_version.sh
@@ -109,20 +78,29 @@ set -x
 #     Install libraries and source files 
 #
    $local && {
-              LIB_DIR4=..
-              LIB_DIR8=..
-              LIB_DIRd=..
-              SRC_DIR=
-             } || {
-              LIB_DIR4=$(dirname $SP_LIB4)
-              LIB_DIR8=$(dirname $SP_LIB8)
-              LIB_DIRd=$(dirname $SP_LIBd)
-              SRC_DIR=$SP_SRC
-              [ -d $LIB_DIR4 ] || mkdir -p $LIB_DIR4
-              [ -d $LIB_DIR8 ] || mkdir -p $LIB_DIR8
-              [ -d $LIB_DIRd ] || mkdir -p $LIB_DIRd
-              [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
-             }
+     instloc=..
+     LIB_DIR4=$instloc
+     LIB_DIR8=$instloc
+     LIB_DIRd=$instloc
+     SRC_DIR=
+   } || {
+     [[ $instloc == --- ]] && {
+       LIB_DIR4=$(dirname $SP_LIB4)
+       LIB_DIR8=$(dirname $SP_LIB8)
+       LIB_DIRd=$(dirname $SP_LIBd)
+       SRC_DIR=$SP_SRC
+     } || {
+       LIB_DIR4=$instloc
+       LIB_DIR8=$instloc
+       LIB_DIRd=$instloc
+       SRC_DIR=$instloc/src
+       [[ $instloc == .. ]] && SRC_DIR=
+     }
+     [ -d $LIB_DIR4 ] || mkdir -p $LIB_DIR4
+     [ -d $LIB_DIR8 ] || mkdir -p $LIB_DIR8
+     [ -d $LIB_DIRd ] || mkdir -p $LIB_DIRd
+     [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
+   }
 
    make clean LIB=
    make install LIB=$spLib4 LIB_DIR=$LIB_DIR4 SRC_DIR=
