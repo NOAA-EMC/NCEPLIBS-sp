49a9b64718bce2c5ee3da10a9256115614498226
Edward Hartnett <edward.hartnett@noaa.gov>
2023-01-05 18:03:33 +0100

    more doxygen

--- src/sptranf.f
+++ src/sptranf.f
@@ -1,61 +1,38 @@
 C> @file
+C> @brief Perform a scalar spherical transform
 C>
-C> Perform a scalar spherical transform
+C> ### Program History Log
+C> Date | Programmer | Comments
+C> -----|------------|---------
+C> 96-02-29 | IREDELL | Initial.
+C> 1998-12-15 | IREDELL | Generic fft used, openmp directives inserted
+C> 2013-01-16 | IREDELL MIRVIS | Fixing afft negative sharing effect
+C>
 C> @author IREDELL @date 96-02-29
 
 C> This subprogram performs a spherical transform between spectral
 C> coefficients of scalar quantities and fields on a global
-C> cylindrical grid.  The wave-space can be either triangular or
+C> cylindrical grid.
+C>
+C> The wave-space can be either triangular or
 C> rhomboidal.  The grid-space can be either an equally-spaced grid
 C> (with or without pole points) or a gaussian grid.
+C>
 C> The wave and grid fields may have general indexing,
 C> but each wave field is in sequential 'ibm order',
 C> i.e. with zonal wavenumber as the slower index.
+C>
 C> Transforms are done in latitude pairs for efficiency;
 C> thus grid arrays for each hemisphere must be passed.
+C>
 C> If so requested, just a subset of the latitude pairs
 C> may be transformed in each invocation of the subprogram.
 C> The transforms are all multiprocessed over latitude except
 C> the transform from fourier to spectral is multiprocessed
 C> over zonal wavenumber to ensure reproducibility.
+C>
 C> Transform several fields at a time to improve vectorization.
 C> Subprogram can be called from a multiprocessing environment.
-C>
-C> PROGRAM HISTORY LOG:
-C> -  96-02-29  IREDELL
-C> - 1998-12-15  IREDELL  GENERIC FFT USED, OPENMP DIRECTIVES INSERTED
-C> - 2013-01-16  IREDELL MIRVIS FIXING AFFT NEGATIVE SHARING EFFECT DURING
-C>			OMP LOOPS BY CREATING TMP AFFT COPY (AFFT_TMP)
-C>			TO BE PRIVATE DURING OMP LOOP THREADING	
-C>
-C> @param IROMB    - INTEGER SPECTRAL DOMAIN SHAPE
-C>                (0 FOR TRIANGULAR, 1 FOR RHOMBOIDAL)
-C> @param MAXWV    - INTEGER SPECTRAL TRUNCATION
-C> @param IDRT     - INTEGER GRID IDENTIFIER
-C>                (IDRT=4 FOR GAUSSIAN GRID,
-C>                 IDRT=0 FOR EQUALLY-SPACED GRID INCLUDING POLES,
-C>                 IDRT=256 FOR EQUALLY-SPACED GRID EXCLUDING POLES)
-C> @param IMAX     - INTEGER EVEN NUMBER OF LONGITUDES.
-C> @param JMAX     - INTEGER NUMBER OF LATITUDES.
-C> @param KMAX     - INTEGER NUMBER OF FIELDS TO TRANSFORM.
-C> @param IP       - INTEGER LONGITUDE INDEX FOR THE PRIME MERIDIAN
-C> @param IS       - INTEGER SKIP NUMBER BETWEEN LONGITUDES
-C> @param JN       - INTEGER SKIP NUMBER BETWEEN N.H. LATITUDES FROM NORTH
-C> @param JS       - INTEGER SKIP NUMBER BETWEEN S.H. LATITUDES FROM SOUTH
-C> @param KW       - INTEGER SKIP NUMBER BETWEEN WAVE FIELDS
-C> @param KG       - INTEGER SKIP NUMBER BETWEEN GRID FIELDS
-C> @param JB       - INTEGER LATITUDE INDEX (FROM POLE) TO BEGIN TRANSFORM
-C> @param JE       - INTEGER LATITUDE INDEX (FROM POLE) TO END TRANSFORM
-C> @param JC       - INTEGER NUMBER OF CPUS OVER WHICH TO MULTIPROCESS
-C> @param[out] WAVE     - REAL (*) WAVE FIELDS IF IDIR>0
-C> @param[out] GRIDN    - REAL (*) N.H. GRID FIELDS (STARTING AT JB) IF IDIR<0
-C> @param[out] GRIDS    - REAL (*) S.H. GRID FIELDS (STARTING AT JB) IF IDIR<0
-C> @param IDIR     - INTEGER TRANSFORM FLAG
-C>                (IDIR>0 FOR WAVE TO GRID, IDIR<0 FOR GRID TO WAVE)
-C>
-C> SUBPROGRAMS CALLED:
-C>  - sptranf0()     sptranf() spectral initialization
-C>  - sptranf1()     sptranf() spectral transform
 C>
 C> Minimum grid dimensions for unaliased transforms to spectral:
 C>   DIMENSION                    |LINEAR              |QUADRATIC
@@ -68,6 +45,32 @@ C>   JMAX (IDRT=0,IROMB=1)        | 4*MAXWV+3          | 5*MAXWV/2*2+3
 C>   JMAX (IDRT=256,IROMB=0)      | 2*MAXWV+1          | 3*MAXWV/2*2+1
 C>   JMAX (IDRT=256,IROMB=1)      | 4*MAXWV+1          | 5*MAXWV/2*2+1
 C>
+C> @param IROMB SPECTRAL DOMAIN SHAPE
+C> (0 FOR TRIANGULAR, 1 FOR RHOMBOIDAL)
+C> @param MAXWV SPECTRAL TRUNCATION
+C> @param IDRT GRID IDENTIFIER
+C> - IDRT=4 FOR GAUSSIAN GRID,
+C> - IDRT=0 FOR EQUALLY-SPACED GRID INCLUDING POLES,
+C> - IDRT=256 FOR EQUALLY-SPACED GRID EXCLUDING POLES
+C> @param IMAX EVEN NUMBER OF LONGITUDES.
+C> @param JMAX NUMBER OF LATITUDES.
+C> @param KMAX NUMBER OF FIELDS TO TRANSFORM.
+C> @param IP LONGITUDE INDEX FOR THE PRIME MERIDIAN
+C> @param IS SKIP NUMBER BETWEEN LONGITUDES
+C> @param JN SKIP NUMBER BETWEEN N.H. LATITUDES FROM NORTH
+C> @param JS SKIP NUMBER BETWEEN S.H. LATITUDES FROM SOUTH
+C> @param KW SKIP NUMBER BETWEEN WAVE FIELDS
+C> @param KG SKIP NUMBER BETWEEN GRID FIELDS
+C> @param JB LATITUDE INDEX (FROM POLE) TO BEGIN TRANSFORM
+C> @param JE LATITUDE INDEX (FROM POLE) TO END TRANSFORM
+C> @param JC NUMBER OF CPUS OVER WHICH TO MULTIPROCESS
+C> @param[out] WAVE WAVE FIELDS IF IDIR>0
+C> @param[out] GRIDN N.H. GRID FIELDS (STARTING AT JB) IF IDIR<0
+C> @param[out] GRIDS S.H. GRID FIELDS (STARTING AT JB) IF IDIR<0
+C> @param IDIR TRANSFORM FLAG
+C> (IDIR>0 FOR WAVE TO GRID, IDIR<0 FOR GRID TO WAVE)
+C>
+C> @author IREDELL @date 96-02-29
       SUBROUTINE SPTRANF(IROMB,MAXWV,IDRT,IMAX,JMAX,KMAX,
      &                   IP,IS,JN,JS,KW,KG,JB,JE,JC,
      &                   WAVE,GRIDN,GRIDS,IDIR)
