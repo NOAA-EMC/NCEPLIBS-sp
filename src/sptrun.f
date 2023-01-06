c2f668155bae20c007a5cc3b9dc6da22c6e346f1
Alex Richert <alexander.richert@noaa.gov>
2023-01-06 08:41:30 -0800

    Doxygen capitalization updates for sptrun and splat; move doxygen table in sptrun.f

--- src/sptrun.f
+++ src/sptrun.f
@@ -6,22 +6,33 @@ C> This subprogram spectrally truncates scalar fields on a global
 C> cylindrical grid, returning the fields to a possibly different
 C> global cylindrical grid. The wave-space can be either triangular
 C> or rhomboidal. either grid-space can be either an equally-spaced
-C> grid (with or without pole points) or a gaussian grid. the grid
+C> grid (with or without pole points) or a Gaussian grid. the grid
 C> fields may have general indexing. the transforms are all
 C> multiprocessed. Transform several fields at a time to improve
 C> vectorization. Subprogram can be called from a multiprocessing
 C> environment.
 C>
+C> Remarks: Minimum grid dimensions for unaliased transforms to spectral:
+C>   Dimension                 |  Linear       |     Quadratic
+C>   -----------------------   |  ---------    |     -------------
+C>   IMAX                      |  2*MAXWV+2    |     3*MAXWV/2*2+2
+C>   JMAX (IDRT=4,IROMB=0)     |  1*MAXWV+1    |     3*MAXWV/2+1
+C>   JMAX (IDRT=4,IROMB=1)     |  2*MAXWV+1    |     5*MAXWV/2+1
+C>   JMAX (IDRT=0,IROMB=0)     |  2*MAXWV+3    |     3*MAXWV/2*2+3
+C>   JMAX (IDRT=0,IROMB=1)     |  4*MAXWV+3    |     5*MAXWV/2*2+3
+C>   JMAX (IDRT=256,IROMB=0)   |  2*MAXWV+1    |     3*MAXWV/2*2+1
+C>   JMAX (IDRT=256,IROMB=1)   |  4*MAXWV+1    |     5*MAXWV/2*2+1
+C>
 C> @param IROMB Spectral domain shape (0 for triangular, 1 for rhomboidal)
 C> @param MAXWV Spectral truncation
 C> @param IDRTI Input grid identifier
-C>  - IDRTI=4 for gaussian grid
+C>  - IDRTI=4 for Gaussian grid
 C>  - IDRTI=0 for equally-spaced grid including poles
 C>  - IDRTI=256 for equally-spaced grid excluding poles
 C> @param IMAXI Even number of input longitudes
 C> @param JMAXI Number of input latitudes
 C> @param IDRTO Output grid identifier
-C>  - IDRTO=4 for gaussian grid
+C>  - IDRTO=4 for Gaussian grid
 C>  - IDRTO=0 for equally-spaced grid including poles
 C>  - IDRTO=256 for equally-spaced grid excluding poles
 C> @param IMAXO Even number of output longitudes
@@ -36,20 +47,10 @@ C> @param KSKIPI Skip number between input grid fields (defaults to IMAXI*JMAXI 
 C> @param ISKIPO Skip number between output longitudes (defaults to 1 if ISKIPO=0)
 C> @param JSKIPO Skip number between output latitudes from south (defaults to -IMAXO if JSKIPO=0)
 C> @param KSKIPO Skip number between output grid fields (defaults to IMAXO*JMAXO if KSKIPO=0)
-C> @param JCPU Number of cpus over which to multiprocess (defaults to environment NCPUS if JCPU=0)
+C> @param JCPU Number of CPUs over which to multiprocess (defaults to environment NCPUS if JCPU=0)
 C> @param GRIDI Input grid fields
 C> @param GRIDO Output grid fields (may overlay input fields if grid shape is appropriate)
 C>
-C> Remarks: Minimum grid dimensions for unaliased transforms to spectral:
-C>   Dimension                 |  Linear       |     Quadratic
-C>   -----------------------   |  ---------    |     -------------
-C>   IMAX                      |  2*MAXWV+2    |     3*MAXWV/2*2+2
-C>   JMAX (IDRT=4,IROMB=0)     |  1*MAXWV+1    |     3*MAXWV/2+1
-C>   JMAX (IDRT=4,IROMB=1)     |  2*MAXWV+1    |     5*MAXWV/2+1
-C>   JMAX (IDRT=0,IROMB=0)     |  2*MAXWV+3    |     3*MAXWV/2*2+3
-C>   JMAX (IDRT=0,IROMB=1)     |  4*MAXWV+3    |     5*MAXWV/2*2+3
-C>   JMAX (IDRT=256,IROMB=0)   |  2*MAXWV+1    |     3*MAXWV/2*2+1
-C>   JMAX (IDRT=256,IROMB=1)   |  4*MAXWV+1    |     5*MAXWV/2*2+1
 C> @author IREDELL @date 96-02-29
       SUBROUTINE SPTRUN(IROMB,MAXWV,IDRTI,IMAXI,JMAXI,IDRTO,IMAXO,JMAXO,
      &                  KMAX,IPRIME,ISKIPI,JSKIPI,KSKIPI,
