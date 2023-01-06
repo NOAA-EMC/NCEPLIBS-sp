28569e5ac8b9eba35d9f055e56bbd03b7d9ee666
Alex Richert <alexander.richert@noaa.gov>
2023-01-06 13:42:58 -0800

    Update doxygen for sptgpsd.f

--- src/sptgpsd.f
+++ src/sptgpsd.f
@@ -1,16 +1,18 @@
 C> @file
-C>
-C> Transform spectral to polar stereo. gradients
+C> @brief Transform spectral to polar stereo. gradients
+C> ### Program history log:
+C>  96-02-29 | IREDELL | Initial
+C> 1998-12-15 | IREDELL | OpenMP directives inserted.
 C> @author IREDELL @date 96-02-29
 
-C> this subprogram performs a spherical transform
+C> This subprogram performs a spherical transform
 C> from spectral coefficients of scalar fields
 C> to gradient fields on a pair of polar stereographic grids.
 C> The wave-space can be either triangular or rhomboidal.
 C> The wave and grid fields may have general indexing,
 C> but each wave field is in sequential 'ibm order',
-C> i.e. with zonal wavenumber as the slower index.
-C> the two square polar stereographic grids are centered
+C> i.e., with zonal wavenumber as the slower index.
+C> The two square polar stereographic grids are centered
 C> on the respective poles, with the orientation longitude
 C> of the southern hemisphere grid 180 degrees opposite
 C> that of the northern hemisphere grid.
@@ -31,37 +33,30 @@ C> The transforms are all multiprocessed over sector points.
 C> transform several fields at a time to improve vectorization.
 C> Subprogram can be called from a multiprocessing environment.
 C>
-C> PROGRAM HISTORY LOG:
-C> -  96-02-29  IREDELL
-C> - 1998-12-15  IREDELL  OPENMP DIRECTIVES INSERTED
+C> @param IROMB Spectral domain shape
+C> (0 for triangular, 1 for rhomboidal)
+C> @param MAXWV Spectral truncation
+C> @param KMAX Number of fields to transform
+C> @param NPS Odd order of the polar stereographic grids
+C> @param KWSKIP Skip number between wave fields
+C> (defaults to (MAXWV+1)*((IROMB+1)*MAXWV+2) if KWSKIP=0)
+C> @param KGSKIP Skip number between grid fields
+C> (defaults to NPS*NPS if KGSKIP=0)
+C> @param NISKIP Skip number between grid i-points
+C> (defaults to 1 if NISKIP=0)
+C> @param NJSKIP Skip number between grid j-points
+C> (defaults to NPS if NJSKIP=0)
+C> @param TRUE Latitude at which PS grid is true (usually 60.)
+C> @param XMESH Grid length at true latitude (M)
+C> @param ORIENT Longitude at bottom of northern PS grid
+C> (southern PS grid will have opposite orientation.)
+C> @param WAVE Wave fields
+C> @param XN Northern polar stereographic x-gradients
+C> @param YN Northern polar stereographic y-gradients
+C> @param XS Southern polar stereographic x-gradients
+C> @param YS Southern polar stereographic y-gradients
 C>
-C> @param IROMB    - INTEGER SPECTRAL DOMAIN SHAPE
-C>                (0 FOR TRIANGULAR, 1 FOR RHOMBOIDAL)
-C> @param MAXWV    - INTEGER SPECTRAL TRUNCATION
-C> @param KMAX     - INTEGER NUMBER OF FIELDS TO TRANSFORM.
-C> @param NPS      - INTEGER ODD ORDER OF THE POLAR STEREOGRAPHIC GRIDS
-C> @param KWSKIP   - INTEGER SKIP NUMBER BETWEEN WAVE FIELDS
-C>                (DEFAULTS TO (MAXWV+1)*((IROMB+1)*MAXWV+2) IF KWSKIP=0)
-C> @param KGSKIP   - INTEGER SKIP NUMBER BETWEEN GRID FIELDS
-C>                (DEFAULTS TO NPS*NPS IF KGSKIP=0)
-C> @param NISKIP   - INTEGER SKIP NUMBER BETWEEN GRID I-POINTS
-C>                (DEFAULTS TO 1 IF NISKIP=0)
-C> @param NJSKIP   - INTEGER SKIP NUMBER BETWEEN GRID J-POINTS
-C>                (DEFAULTS TO NPS IF NJSKIP=0)
-C> @param TRUE     - REAL LATITUDE AT WHICH PS GRID IS TRUE (USUALLY 60.)
-C> @param XMESH    - REAL GRID LENGTH AT TRUE LATITUDE (M)
-C> @param ORIENT   - REAL LONGITUDE AT BOTTOM OF NORTHERN PS GRID
-C>                (SOUTHERN PS GRID WILL HAVE OPPOSITE ORIENTATION.)
-C> @param WAVE     - REAL (*) WAVE FIELDS
-C> @param XN       - REAL (*) NORTHERN POLAR STEREOGRAPHIC X-GRADIENTS
-C> @param YN       - REAL (*) NORTHERN POLAR STEREOGRAPHIC Y-GRADIENTS
-C> @param XS       - REAL (*) SOUTHERN POLAR STEREOGRAPHIC X-GRADIENTS
-C> @param YS       - REAL (*) SOUTHERN POLAR STEREOGRAPHIC Y-GRADIENTS
-C>
-C> SUBPROGRAMS CALLED:
-C>   - SPWGET()       GET WAVE-SPACE CONSTANTS
-C>   - SPLAPLAC()     COMPUTE LAPLACIAN IN SPECTRAL SPACE
-C>   - SPTGPSV()      TRANSFORM SPECTRAL VECTOR TO POLAR STEREO.
+C> @author IREDELL @date 96-02-29
       SUBROUTINE SPTGPSD(IROMB,MAXWV,KMAX,NPS,
      &                   KWSKIP,KGSKIP,NISKIP,NJSKIP,
      &                   TRUE,XMESH,ORIENT,WAVE,XN,YN,XS,YS)
